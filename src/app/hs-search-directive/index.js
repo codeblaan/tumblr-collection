783457fc64a13d5f20f1977b9535c0c14edffe34
blaan <blaan@protonmail.com>
2016-12-12 20:17:27 -0800

    Add search by tag

--- src/app/hs-search-directive/index.js
+++ src/app/hs-search-directive/index.js
@@ -2,9 +2,14 @@ export default function(tumblr) {
   return {
     template: require('./index.html'),
     link: (scope) => {
+      scope.blog = {};
       scope.results = {};
-      scope.search = () => {
-        tumblr.getPosts(scope.blog.name).then(posts => {
+      scope.search = (blog) => {
+        if (!blog.name && !blog.tag) {
+          scope.results.posts = [];
+          return;
+        }
+        tumblr.getPosts(blog.name, blog.tag).then(posts => {
           scope.results.posts = posts;
         }, error => {
           scope.results.posts = [];
--- src/app/tumblr-service/index.js
+++ src/app/tumblr-service/index.js
@@ -1,19 +1,36 @@
 export default function($http, $sce) {
   const API_KEY = 'ifJyOYcIVoAYQ9PyHF1nCYDkMOS1phoXZpei5ZIRnMUJJfXQhY';
-  const BASE_URL = 'https://api.tumblr.com/v2/blog';
+  const BASE_URL = 'https://api.tumblr.com/v2';
 
   return {
     getPosts: getPosts
   }
 
-  function getPosts(blogName, offset=0) {
-    return $http
-      .jsonp(resourceUrl(blogName, offset))
-      .then(response => response.data.response.posts, error => []);
+  function getPosts(blogName, blogTag, offset=0) {
+    if (useTaggedApi(blogName, blogTag)) {
+      return $http
+        .jsonp(taggedApiUrl(blogTag, offset))
+        .then(response => response.data.response, error => []);
+    } else {
+      return $http
+        .jsonp(blogApiUrl(blogName, blogTag, offset))
+        .then(response => response.data.response.posts, error => []);
+    }
+  }
+  function useTaggedApi(blogName, blogTag) {
+    return !blogName && blogTag;
   }
 
-  function resourceUrl(blogName, offset) {
-    var url = `${BASE_URL}/${blogName}.tumblr.com/posts?offset=${offset}&api_key=${API_KEY}`;
+  function taggedApiUrl(blogTag) {
+    var url = `${BASE_URL}/tagged?tag=${blogTag}&api_key=${API_KEY}`;
+    return $sce.trustAsResourceUrl(url);
+  }
+
+  function blogApiUrl(blogName, blogTag, offset) {
+    var url = `${BASE_URL}/blog/${blogName}.tumblr.com/posts?offset=${offset}&api_key=${API_KEY}`;
+    if (blogTag) {
+      url += `&tag=${blogTag}`;
+    }
     return $sce.trustAsResourceUrl(url);
   }
 }
